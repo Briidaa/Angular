2c909b5205341db755ad80c241b3546a67f48dde
Briidaa <ntulibrian93@gmail.com>
2022-07-17 12:10:24 +0200

    dashboard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './Components/login/login.component';
-import { RegisterComponent } from './Components/register/register.component';
+import { IndexComponent } from './Components/index/index.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { TablesComponent } from './pages/tables/tables.component';
+
 
 const routes: Routes = [
-  {path:'',component:RegisterComponent},
-  {path:'login',component:LoginComponent}
+{path:'',component: RegisterComponent},
+{path:'login', component: LoginComponent},
+{
+  path: 'admin',
+  component:IndexComponent,
+  children: [
+    {path:'', component: DashboardComponent},
+    {path: 'tables',component: TablesComponent}
+  ]
+}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './Components/register/register.component';
-import { LoginComponent } from './Components/login/login.component';
-
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { TopbarComponent } from './Components/topbar/topbar.component';
+import { PageContentComponent } from './Components/page-content/page-content.component';
+import { IndexComponent } from './Components/index/index.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { TablesComponent } from './pages/tables/tables.component';
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    LoginComponent
+    SidebarComponent,
+    TopbarComponent,
+    PageContentComponent,
+    IndexComponent,
+    DashboardComponent,
+    TablesComponent
   ],
   imports: [
     BrowserModule,
