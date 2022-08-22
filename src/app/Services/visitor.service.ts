9b5bdfb6797f28a001943398663bdeb3cf9ef4ba
Mashotolucky <mashotolucky99@gmail.com>
2022-08-22 09:55:33 +0200

    ENV added

--- src/app/Services/visitor.service.ts
+++ src/app/Services/visitor.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VisitorService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  devbaseUrl: 'http://localhost:4000/api/visitor/'
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
